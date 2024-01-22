b2b5c9e7c8a46540d50872ecefa26d9460a67368
Eva <47476219+Biel333@users.noreply.github.com>
2024-01-22 21:28:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import config from './config.js';
 const app = express();
 let server = null;
 
+console.log("hello, ci/cd");
+
 app.get('/hello/:name', (req, res) => {
   const { name } = req.params;
 
